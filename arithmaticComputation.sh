ba456f659afe54075edc806133a55059c090e5ec
unknown <sujithds99@gmail.com>
2021-06-04 15:48:42 +0530

    Result in Descending Order

--- arithmaticComputation.sh
+++ arithmaticComputation.sh
@@ -10,6 +10,27 @@ arithmeticOp2=$(((a*b)+c))
 arithmeticOp3=$((c+(a/b)))
 arithmeticOp4=$(((a%b)+c))
 
+function arraySortDescending() {
+	arrLength=${#resultArray[@]}
+	for (( i=0; i<$arrLength; i++ ))
+	do
+		min=$i
+		for (( j=i+1; j<$arrLength; j++ ))
+		do
+			arrayValue1=${resultArray[j]}
+			arrayValue2=${resultArray[min]}
+			if [ $arrayValue1 -gt $arrayValue2 ]
+			then
+				min=$j
+			fi
+		done
+		temp=${resultArray[i]}
+		resultArray[i]=${resultArray[min]}
+		resultArray[min]=$temp
+	done
+	echo "Sorted resultArray in Descending order: ${resultArray[@]}"
+}
+
 result[0]=$arithmeticOp1
 result[1]=$arithmeticOp2
 result[2]=$arithmeticOp3
@@ -19,4 +40,7 @@ for (( i=0; i<${#result[@]}; i++ ))
 do
 	resultArray[i]=${result[$i]}
 done
+
 echo "resultArray: ${resultArray[@]}"
+
+arraySortDescending
